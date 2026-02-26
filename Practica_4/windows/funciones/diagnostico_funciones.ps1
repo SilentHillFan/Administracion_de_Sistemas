e9120e09dec5aa7e6e783be8a48ac33f43123775
Humberto Ayala <ayalahumberto200@gmail.com>
2026-02-26 14:45:44 -0700

    Refactor: migracion de logica diagnostico Powershell a funciones modulares

---
+++ Practica_4/windows/funciones/diagnostico_funciones.ps1
@@ -0,0 +1,12 @@
+# diagnostico_funciones.ps1
+
+function Mostrar-Diagnostico {
+    echo "Nombre del equipo:"
+    $env:COMPUTERNAME
+
+    echo "IP:"
+    (Get-NetIPAddress -InterfaceIndex 4 -AddressFamily IPv4).IPAddress
+
+    echo "Espacio en disco (GB):"
+    (Get-Volume -DriveLetter C).SizeRemaining / 1GB
+}
